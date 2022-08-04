d506c094e4e0b0d16cd38ced9e59d706dcebc8eb
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-04 11:23:35 +0700

    Add hash and blockHash function

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -21,11 +23,22 @@ class Block {
 
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
-        const lastHash = lastBlock.lastHash;
-        const hash = 'todo-hash';
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
     }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${ timestamp }${ lastHash }${ data }`).toString();
+    }
+
+    static blockHash(block) {
+        const { timestamp, lastHash, data } = block;
+        
+        return Block.hash(timestamp, lastHash, data);
+    }
+
 }
 
 module.exports = Block;
