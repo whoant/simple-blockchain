c824d99f19df2282d6c31f69bfb02f1f719aa3c9
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-04 23:56:21 +0700

    Server blockchain

---
+++ app/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const Blockchain = require('../blockchain/blockchain');
+const P2pServer = require('./p2p-server');
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const app = express();
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
+app.use(express.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const newBlock = bc.addBlock(req.body.data);
+    console.log(`New block addded: ${ newBlock.toString() }`)
+    p2pServer.syncChain();
+    res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${ HTTP_PORT }`));
+p2pServer.listen();
