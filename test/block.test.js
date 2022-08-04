cc0aeb4964b629f0d90313ba00e39f239122e0d4
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-04 23:56:44 +0700

    Unit test for block

--- test/block.test.js
+++ test/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('../blockchain/block');
+const {DIFFICULTY} = require("../config");
 
 describe('Block', () => {
 
@@ -7,13 +8,26 @@ describe('Block', () => {
         data = 'Tuan';
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
-    })
+    });
 
     it('sets the `data` to match the input', () => {
-        expect(block.data).toEqual(data)
+        expect(block.data).toEqual(data);
     });
 
     it('set the `lastHash` to match the hash of the last block', () => {
-        expect(block.lastHash).toEqual(lastBlock.hash)
+        expect(block.lastHash).toEqual(lastBlock.hash);
     });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+    });
+
+    it('raises the difficulty for quickly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
+    });
+
 });
