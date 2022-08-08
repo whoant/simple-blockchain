fda0131d3eba21885e8f16e5e7c13ac2aa1c5637
whoant <vovanhoangtuan4.2@gmail.com>
2022-08-08 13:41:41 +0700

    Message handler

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,30 +2,35 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+const MESSAGE_TYPES = {
+    chain: "CHAIN",
+    transaction: "TRANSACTION"
+};
 
 class P2pServer {
-    constructor(blockchain) {
+    constructor(blockchain, transactionPool){
         this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
         this.sockets = [];
     }
 
-    listen() {
+    listen(){
         const server = new Websocket.WebSocketServer({
             port: P2P_PORT
         });
         this.connectToPeers();
         server.on('connection', socket => this.connectSocket(socket));
-        console.log(`Listening for peer-to-peer connections on : ${ P2P_PORT }`);
+        console.log(`Listening for peer-to-peer connections on : ${P2P_PORT}`);
     }
 
-    connectToPeers() {
+    connectToPeers(){
         peers.forEach(peer => {
             const socket = new Websocket(peer);
             socket.on('open', () => this.connectSocket(socket));
         });
     }
 
-    connectSocket(socket) {
+    connectSocket(socket){
         this.sockets.push(socket);
         console.log('Socket connected');
         this.messageHandler(socket);
@@ -34,19 +39,35 @@ class P2pServer {
     }
 
 
-    sendChain(socket) {
-        socket.send(JSON.stringify(this.blockchain.chain));
+    sendChain(socket){
+        socket.send(JSON.stringify({type: MESSAGE_TYPES.chain, chain: this.blockchain.chain}));
     }
 
-    messageHandler(socket) {
+    sendTransaction(socket, transaction){
+        socket.send(JSON.stringify({type: MESSAGE_TYPES.transaction, transaction}));
+    }
+
+    messageHandler(socket){
         socket.on('message', message => {
             const data = JSON.parse(message);
-            this.blockchain.replaceChain(data);
+            switch (data.type) {
+                case MESSAGE_TYPES.chain:
+                    this.blockchain.replaceChain(data.chain);
+                    break;
+                case MESSAGE_TYPES.transaction:
+                    this.transactionPool.updateOrAddTransaction(data.transaction);
+                    break;
+            }
+            
         });
     }
 
-    syncChain() {
+    syncChain(){
         this.sockets.forEach(socket => this.sendChain(socket));
+    }
+
+    broadcastTransaction(transaction){
+        this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
     }
 
 }
